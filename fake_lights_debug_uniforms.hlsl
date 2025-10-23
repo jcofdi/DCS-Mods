2f091b31775bb43a0a9ca055071391880cf2fb96
jcofdi <66176356+jcofdi@users.noreply.github.com>
2025-10-22 22:04:55 -0500

    Add files via upload

--- fake_lights_debug_uniforms.hlsl
+++ fake_lights_debug_uniforms.hlsl
@@ -21,7 +21,7 @@ cbuffer fake_lights_debug_uniforms {
 static const float FL_DBG_SIZE_MULT = 1;	// Multiplier of source size.
 static const float FL_DBG_LUMINANCE_MULT = 1;	// Multiplier of luminance.
 static const float FL_DBG_MIN_SIZE_IN_PIXELS_MULT = 1;	// Self describing.
-static const float FL_DBG_DISTANCE_MULT = 1;	// Self describing.
+static const float FL_DBG_DISTANCE_MULT = 10;	// Arttu - Change number to change the distance. DCS default is 1.
 static const float FL_DBG_scatteringWeight = 1;	// Self describing.
 static const float FL_DBG_softParticleMult = 1;	// Self describing.
 static const float FL_DBG_transparencyVal = 0;
