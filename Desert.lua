c1ddb46bf1337378b351dba8fdce61c0ce987102
jcofdi <66176356+jcofdi@users.noreply.github.com>
2025-10-22 22:37:26 -0500

    Add files via upload

---
+++ Desert.lua
@@ -0,0 +1,136 @@
+surface =
+{
+	['landShadowsSharpness'] = 0.2;
+	['diffuseGain'] = 1.2;
+	['landAOminimal'] = 0.43;
+	['gamma'] = 1;
+	['surfaceColorBias'] = 
+	{
+		['1'] = 0;
+		['4'] = 0;
+		['3'] = 0;
+		['2'] = 0;
+	};
+	['landShadowsPower'] = 0.8;
+	['colorGain'] = 1;
+	['diffuseBias'] = 0.2;
+	['kDiffuse'] = 1.19;
+	['surfaceColorGain'] = 
+	{
+		['1'] = 1;
+		['4'] = 1;
+		['3'] = 1;
+		['2'] = 1;
+	};
+	['normalizeDiffuse'] = 0.35;
+	['kAmbient'] = 1.81;
+};
+objects =
+{
+	['diffuseGain'] = 1.2;
+	['kAmbient'] = 1.1;
+	['gamma'] = 1;
+	['surfaceColorBias'] = 
+	{
+		['1'] = 0;
+		['4'] = 0;
+		['3'] = 0;
+		['2'] = 0;
+	};
+	['colorGain'] = 1.3;
+	['diffuseBias'] = 0.3;
+	['surfaceColorGain'] = 
+	{
+		['1'] = 1;
+		['4'] = 1;
+		['3'] = 1;
+		['2'] = 1;
+	};
+	['normalizeDiffuse'] = 0.45;
+	['kDiffuse'] = 1.13;
+};
+trees =
+{
+	['kAmbient'] = 1.41;
+	['kDiffuse'] = 1.15;
+	['surfaceColorGain'] = 
+	{
+		['1'] = 1;
+		['4'] = 1;
+		['3'] = 1;
+		['2'] = 1;
+	};
+	['gamma'] = 1;
+	['surfaceColorBias'] = 
+	{
+		['1'] = 0;
+		['4'] = 0;
+		['3'] = 0;
+		['2'] = 0;
+	};
+};
+water =
+{
+};
+radar =
+{
+	
+	['summer'] = 
+	{
+		
+		{
+			['color'] = 
+			{
+				1;
+				0;
+				0;
+				1;
+			};
+			['sigma'] = 0;
+		};
+		
+		{
+			['color'] = 
+			{
+				1;
+				0;
+				0;
+				1;
+			};
+			['sigma'] = 0;
+		};
+		
+		{
+			['color'] = 
+			{
+				1;
+				0;
+				0;
+				1;
+			};
+			['sigma'] = 0;
+		};
+		
+		{
+			['color'] = 
+			{
+				0.70098042488098;
+				0.6615651845932;
+				0.47762879729271;
+				1;
+			};
+			['sigma'] = 0.275;
+		};
+		
+		{
+			['color'] = 
+			{
+				1;
+				0;
+				0;
+				1;
+			};
+			['sigma'] = 0;
+		};
+	};
+};
