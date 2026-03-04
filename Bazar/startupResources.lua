85753476f1cb307ef44924d1d95bb2d01bf1c038
jcofdi <66176356+jcofdi@users.noreply.github.com>
2026-03-03 21:27:39 -0600

    Add files via upload

---
+++ Bazar/startupResources.lua
@@ -0,0 +1,6 @@
+explosions = {
+	'Config/Effects/explosions/CBUShot.lua',
+	'Config/Effects/explosions/CBUGround.lua',
+	'Config/Effects/explosions/ShellOnObject.lua',
+	'Config/Effects/explosions/ShellOnGround.lua',	
+}
